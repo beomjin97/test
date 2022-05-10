d1a15187ff719a54456616b478c344561d223f65
beomjin97 <qjawls646@gmail.com>
2022-05-10 14:28:47 +0900

    연동 test

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "혜영 재민") String name) {
+        return String.format("안녕 %s, 지금 시간은 " + new Date() +" 이야" , name);
+    }
+}
